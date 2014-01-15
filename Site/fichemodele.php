25901121c4e4460940820291ab3b1d3a8baaf701
Léo <leodru@hotmail.fr>
2014-01-15 23:35:01 +0100

    j'ai juste rajouté quelques <div> pour préparer le titre du modèle, l'image et la description (le nombre de polys ect...) je m'occuperai du passage entre la base de donnée et le modèle demandé pour que la fiche de modele se mette à jour correctement en fonction du modèle recherché

--- Site/fichemodele.php
+++ Site/fichemodele.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 	
-		<title>Fiche du Modèle</title>
+		<title>Fiche du Modele</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<link rel="stylesheet" media="screen" type="text/css" href="styles/main.css" />
 		
@@ -32,14 +32,26 @@
 		</div>
 		
 		<div id="corps">
+			<div>
+			Titre :
+			</div>
+			<div>
 			<center>
 				<?php
 					$image = $bd->query("SELECT modele FROM test WHERE id=$id");
 					$img = $image->fetch();
 					printf($img["modele"]);
 				?>
+			</br>
 			</center>
+			</div>
+			Description du fichier( auteurs ect... nbre polys...): </br>
+			Jo le roi de la frite.
+			</br>
+
 		</div>
+		
+		
 		
 		<div id="footer">
 		The IUT Copyright - Tous droits réservés © 2013-2014
